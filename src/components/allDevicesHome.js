c13fa83f107dc06cd3f80b0c68977c330849a38e
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-30 22:26:34 +0100

    Create allDevicesHome.js

---
+++ src/components/allDevicesHome.js
@@ -0,0 +1,16 @@
+import * as React from "react"
+import '../styles/global.css';
+
+
+// markup
+const AllDevicesHome = () => {
+  return (
+      <div className=" bg-maybe-dark-background w-full h-screen">
+          
+          <h1 className="text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-7xl text-center font-semibold ">Your digital <br/> daily reminder</h1>
+          
+    </div>
+  )
+}
+
+export default AllDevicesHome

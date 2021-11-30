8aa41e51e5f01568547b4e92956b766c17e64b88
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-30 22:44:29 +0100

    Update allDevicesHome.js

--- src/components/allDevicesHome.js
+++ src/components/allDevicesHome.js
@@ -1,16 +1,35 @@
 import * as React from "react"
 import '../styles/global.css';
 
+//images
+import AboutImg from "../images/devices.png";
 
 // markup
 const AllDevicesHome = () => {
-  return (
-      <div className=" bg-maybe-dark-background w-full h-screen">
-          
-          <h1 className="text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-7xl text-center font-semibold ">Your digital <br/> daily reminder</h1>
-          
-    </div>
-  )
+    return (
+        <div className=" bg-light-background w-full h-screen">
+
+            <div id="about" className="mb-28 p-0 flex flex-col w-full xl:flex-row md:p-16 md:mb-18">
+                <div className="w-full xl:w-full">
+                    <img src={AboutImg} className="pt-6" alt="church" />
+                </div>
+                <div className="flex text-center flex-col w-full px-3 justify-center xl:w-5/10 xl:text-left xl:pl-10">
+
+                    <h2 className="text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-5xl sm:text-6xl md:text-7xl 2xl:text-8xl font-black font-sans text-dark-text py-0">
+                        Hør mere om  <br />
+                       hvem vi er!
+                    </h2>
+                    <p className="text-xl px-5 text-dark-text py-5 md:px-24 xl:pl-0 xl:pr-20">
+                    Our app works on computers, tablets and phones.
+Just download it and register or go to your account
+Information is synchronized between devices.
+The application will tell you about itself and show
+the full functionality of the tooltip.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default AllDevicesHome

b87d45adf70e107fe87d3b8b649bbdb1f416c2e0
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-30 22:46:13 +0100

    Update allDevicesHome.js

--- src/components/allDevicesHome.js
+++ src/components/allDevicesHome.js
@@ -2,7 +2,7 @@ import * as React from "react"
 import '../styles/global.css';
 
 //images
-import AboutImg from "../images/devices.png";
+import DevicesImg from "../images/devices.png";
 
 // markup
 const AllDevicesHome = () => {
@@ -11,13 +11,12 @@ const AllDevicesHome = () => {
 
             <div id="about" className="mb-28 p-0 flex flex-col w-full xl:flex-row md:p-16 md:mb-18">
                 <div className="w-full xl:w-full">
-                    <img src={AboutImg} className="pt-6" alt="church" />
+                    <img src={DevicesImg} className="pt-6" alt="mockup" />
                 </div>
                 <div className="flex text-center flex-col w-full px-3 justify-center xl:w-5/10 xl:text-left xl:pl-10">
 
                     <h2 className="text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-5xl sm:text-6xl md:text-7xl 2xl:text-8xl font-black font-sans text-dark-text py-0">
-                        Hør mere om  <br />
-                       hvem vi er!
+                    Works on all devices
                     </h2>
                     <p className="text-xl px-5 text-dark-text py-5 md:px-24 xl:pl-0 xl:pr-20">
                     Our app works on computers, tablets and phones.

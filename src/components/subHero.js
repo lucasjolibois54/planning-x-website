050925d540517d39617a5b0e14a925167a21a61a
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-30 22:14:31 +0100

    Create subHero.js

---
+++ src/components/subHero.js
@@ -0,0 +1,19 @@
+import * as React from "react"
+import '../styles/global.css';
+
+
+// markup
+const SubHero = () => {
+  return (
+      <div className="">
+                <div className=" bg-yellow-200 w-full h-56">
+          <h2>nnnn</h2>
+    </div>
+    <div className=" bg-white w-full mb-56">
+          <h2>n</h2>
+    </div>
+    </div>
+  )
+}
+
+export default SubHero

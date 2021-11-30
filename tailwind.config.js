4fe2080948a51459d2119252b3caeca42ea04348
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-30 22:31:28 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,28 @@ module.exports = {
       },
     },
   },
+screens: {
+  'xsm': '520px',
+  // => @media (min-width: 520px) { ... }
+
+  'sm': '640px',
+  // => @media (min-width: 640px) { ... }
+
+  'md': '768px',
+  // => @media (min-width: 768px) { ... }
+
+  'lg': '1024px',
+  // => @media (min-width: 1024px) { ... }
+
+  '2lg': '1148px',
+  // => @media (min-width: 1148px) { ... }
+
+  'xl': '1280px',
+  // => @media (min-width: 1280px) { ... }
+
+  '2xl': '1536px',
+  // => @media (min-width: 1536px) { ... }
+},
   variants: {
     extend: {},
   },

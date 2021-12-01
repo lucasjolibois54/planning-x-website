22841823a2e11d78c2c8f308987821989f9d92d9
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-01 04:03:59 +0100

    Create downloadForFree.js

---
+++ src/components/downloadForFree.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import '../styles/global.css';
+
+
+// markup
+const DownloadForFree = () => {
+  return (
+      <div>
+ 
+    </div>
+  )
+}
+
+export default DownloadForFree

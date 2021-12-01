2e7173dc632bacabbda4328febdf5f0784a9cc14
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-01 04:04:02 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import HeroSection from "../components/heroSection"
 import SubHero from "../components/SubHero"
 import AllDevicesHome from "../components/allDevicesHome.js"
 import FeaturedSection from "../components/featuredSection.js"
+import DownloadForFree from "../components/DownloadForFree.js"
 import Footer from "../components/footer.js"
 // styles
 const pageStyles = {
@@ -23,6 +24,7 @@ const IndexPage = () => {
       <SubHero/>
       <AllDevicesHome/>
       <FeaturedSection/>
+      <DownloadForFree/>
       <Footer/>
     </main>
   )

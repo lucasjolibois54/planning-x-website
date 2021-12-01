eb0469ebd2a854c2af5cef34c50bc43b311efb7c
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-01 12:53:01 +0100

    Create privacyPolicy.js

---
+++ src/pages/privacyPolicy.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+import '../styles/global.css';
+
+//images
+
+// markup
+const PravacyPolicy = () => {
+    return (
+        <div className=" ">
+namnam
+        </div>
+    )
+}
+
+export default PravacyPolicy

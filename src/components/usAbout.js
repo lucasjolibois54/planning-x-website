6c4e6b36264937cd987295a1eb475ddb23fd5228
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-01 17:10:26 +0100

    Update usAbout.js

--- src/components/usAbout.js
+++ src/components/usAbout.js
@@ -18,11 +18,7 @@ const UsAbout = () => {
                             Meet the team<br /> behind it!
                         </h2>
                         <p className="text-xl text-light-background px-5 text-dark-text py-5 md:px-24 xl:pl-0 xl:pr-20">
-                            Our app works on computers, tablets and phones.
-                            Just download it and register or go to your account
-                            Information is synchronized between devices.
-                            The application will tell you about itself and show
-                            the full functionality of the tooltip.
+                        We are a team of developers based in Copenhagen. Our team is based on 4 core values; being inspiring, hard-working, helpful, and relying. We provide free software for you to use, with no hidden fees. This is why our customers love us!
                         </p>
                         <div className="flex justify-center text-center items-center py-0 xl:justify-start">
                             <a href="#" className="py-1 px-4 text-light-button border-2 bg-blue-button border-blue-button rounded-md mr-2 transform transition duration-300 ease-in-out hover:scale-110 hover:bg-gray-800 hover:border-gray-800">Download app</a> <a className="ml-2 py-1 px-4 text-light-button border-2 border-light-button hover:text-light-background hover:border-blue-button  transform transition duration-300 ease-in-out  hover:scale-110 rounded-md" href="#">Download for PC</a>

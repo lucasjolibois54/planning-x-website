d1ec0655c812f87bd810ebbd6be23a80b005d839
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-01 02:04:54 +0100

    Update subHero.js

--- src/components/subHero.js
+++ src/components/subHero.js
@@ -1,18 +1,22 @@
 import * as React from "react"
 import '../styles/global.css';
 
+//images
+import SubHeroTablet from "../images/Rectangle276(1).png";
+
 
 // markup
 const SubHero = () => {
   return (
-      <div className="">
-                <div className=" bg-yellow-200 w-full h-56">
-          <h2>nnnn</h2>
-    </div>
-    <div className=" bg-light-background w-full mb-56">
-          <h2>n</h2>
-    </div>
-    </div>
+<div>
+<div className="h-50v bg-maybe-dark-background"></div>
+<div className="relative">
+<div className="w-full xl:w-6/12 absolute  left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                    <img src={SubHeroTablet} className="pt-6" alt="mockup" />
+                </div>
+                </div>
+<div className="h-50v bg-light-background"></div>
+</div>
   )
 }
 

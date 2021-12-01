091947537a888b0281f0435d6782b328bbd580e9
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-01 14:19:10 +0100

    Update termOfService.js

--- src/pages/termOfService.js
+++ src/pages/termOfService.js
@@ -8,60 +8,105 @@ import Footer from "../components/footer.js"
 const TermOfService = () => {
     return (
         <div className="bg-maybe-dark-background w-full h-screen">
-            <div className="mx-4 text-left text-white bg-maybe-dark-background pt-20 transform pb-36 policy-text">
-                <h1 className="text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-4xl md:text-7xl text-center font-semibold ">Planner X <br /> Privacy Policy</h1>
-                <h2 className="font-light text-lg pt-10 md:w-full mb-12 pb-38">
-                    This privacy policy will help you understand how Planner X uses and
-                    protects the data you provide to us when you visit and use plannerx.com.
-                    We reserve the right to change this policy at any given time, of which you will be promptly
-                    updated. If you want to make sure that you are up to date with the latest changes, we advise
-                    you to frequently visit this page.
-                    What User Data We Collect <br/><br/>
-                    When you visit the website, we may collect the following data:<br/>
-                    • Your IP address.<br/>
-                    • Your contact information and email address.<br/>
-                    • Other information such as interests and preferences.<br/>
-                    • Data profile regarding your online behavior on our website.
-                    Why We Collect Your Data<br/><br/>
-                    We are collecting your data for several reasons:<br/>
-                    • To better understand your needs.<br/>
-                    • To improve our services and products.<br/>
-                    • To send you promotional emails containing the information we think you will find
-                    interesting.<br/>
-                    • To contact you to fill out surveys and participate in other types of market research.<br/>
-                    • To customize our website according to your online behavior and personal preferences.<br/><br/>
-                    Safeguarding and Securing the Data
-                    Planner X is committed to securing your data and keeping it confidential. Planner X has done all in its
-                    power to prevent data theft, unauthorized access, and disclosure by implementing the latest
-                    technologies and software, which help us safeguard all the information we collect online.
-                    <br/> <br/>
-                    Our Cookie Policy
-                    Once you agree to allow our website to use cookies, you also agree to use the data it collects
-                    regarding your online behavior (analyze web traffic, web pages you spend the most time on,
-                    and websites you visit).
-                    The data we collect by using cookies is used to customize our website to your needs. After we
-                    use the data for statistical analysis, the data is completely removed from our systems.
-                    Please note that cookies don't allow us to gain control of your computer in any way. They are
-                    strictly used to monitor which pages you find useful and which you do not so that we can
-                    provide a better experience for you.
-                    <br/><br/>
-                    If you want to disable cookies, you can do it by accessing the settings of your internet browser.
-                    (Provide links for cookie settings for major internet browsers).
-                    Links to Other Websites<br/><br/>
-                    Our website contains links that lead to other websites. If you click on these links Planner X is not
-                    held responsible for your data and privacy protection. Visiting those websites is not governed by
-                    this privacy policy agreement. Make sure to read the privacy policy documentation of the
-                    website you go to from our website.
-                    Restricting the Collection of your Personal Data
-                    At some point, you might wish to restrict the use and collection of your personal data. You can
-                    achieve this by doing the following:
-                    When you are filling the forms on the website, make sure to check if there is a box which you
-                    can leave unchecked, if you don't want to disclose your personal information.
-                    If you have already agreed to share your information with us, feel free to contact us via email
-                    and we will be more than happy to change this for you.
-                    Planner X will not lease, sell or distribute your personal information to any third parties, unless we
-                    have your permission. We might do so if the law forces us. Your personal information will be
-                    used when we need to send you promotional materials if you agree to this privacy policy.</h2>
+            <div className="mx-4 text-left text-white bg-maybe-dark-background pt-20 md:pt-28 transform pb-36 policy-text">
+                <h1 className="text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-4xl md:text-6xl text-center font-semibold ">Planner X <br /> Terms & Conditions</h1>
+                <h2 className="font-light text-lg pt-10 md:pt-16 md:w-full mb-12 pb-38">
+                Terms and Conditions
+Welcome to Planner X! <br/><br/>
+
+These terms and conditions outline the rules and regulations for the use of Planner X's Website, located at plannerx.com. <br/><br/>
+
+By accessing this website we assume you accept these terms and conditions. Do not continue to use Planner X if you do not agree to take all of the terms and conditions stated on this page.
+
+The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company’s terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same. <br/><br/>
+
+Cookies
+We employ the use of cookies. By accessing Planner X, you agreed to use cookies in agreement with the Planner X's Privacy Policy.<br/><br/>
+
+Most interactive websites use cookies to let us retrieve the user’s details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.<br/><br/>
+
+License
+Unless otherwise stated, Planner X and/or its licensors own the intellectual property rights for all material on Planner X. All intellectual property rights are reserved. You may access this from Planner X for your own personal use subjected to restrictions set in these terms and conditions.<br/><br/>
+
+You must not:<br/>
+
+Republish material from Planner X<br/>
+Sell, rent or sub-license material from Planner X<br/>
+Reproduce, duplicate or copy material from Planner X<br/>
+Redistribute content from Planner X<br/>
+This Agreement shall begin on the date hereof.<br/><br/>
+
+Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website. Planner X does not filter, edit, publish or review Comments prior to their presence on the website. Comments do not reflect the views and opinions of Planner X,its agents and/or affiliates. Comments reflect the views and opinions of the person who post their views and opinions. To the extent permitted by applicable laws, Planner X shall not be liable for the Comments or for any liability, damages or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of the Comments on this website.<br/><br/>
+
+Planner X reserves the right to monitor all Comments and to remove any Comments which can be considered inappropriate, offensive or causes breach of these Terms and Conditions.<br/><br/>
+
+You warrant and represent that:<br/>
+
+You are entitled to post the Comments on our website and have all necessary licenses and consents to do so;<br/>
+The Comments do not invade any intellectual property right, including without limitation copyright, patent or trademark of any third party;<br/>
+The Comments do not contain any defamatory, libelous, offensive, indecent or otherwise unlawful material which is an invasion of privacy<br/>
+The Comments will not be used to solicit or promote business or custom or present commercial activities or unlawful activity.<br/>
+You hereby grant Planner X a non-exclusive license to use, reproduce, edit and authorize others to use, reproduce and edit any of your Comments in any and all forms, formats or media.<br/>
+
+Hyperlinking to our Content<br/>
+The following organizations may link to our Website without prior written approval:<br/><br/>
+
+Government agencies;<br/>
+Search engines;<br/>
+News organizations;<br/>
+Online directory distributors may link to our Website in the same manner as they hyperlink to the Websites of other listed businesses; and<br/>
+System wide Accredited Businesses except soliciting non-profit organizations, charity shopping malls, and charity fundraising groups which may not hyperlink to our Web site.<br/>
+These organizations may link to our home page, to publications or to other Website information so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products and/or services; and (c) fits within the context of the linking party’s site.<br/><br/>
+
+We may consider and approve other link requests from the following types of organizations:<br/><br/>
+
+commonly-known consumer and/or business information sources;<br/>
+dot.com community sites;<br/>
+associations or other groups representing charities;<br/>
+online directory distributors;<br/>
+internet portals;<br/>
+accounting, law and consulting firms; and<br/>
+educational institutions and trade associations.<br/>
+We will approve link requests from these organizations if we decide that: (a) the link would not make us look unfavorably to ourselves or to our accredited businesses; (b) the organization does not have any negative records with us; (c) the benefit to us from the visibility of the hyperlink compensates the absence of Planner X; and (d) the link is in the context of general resource information.<br/><br/>
+
+These organizations may link to our home page so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products or services; and (c) fits within the context of the linking party’s site.<br/><br/>
+
+If you are one of the organizations listed in paragraph 2 above and are interested in linking to our website, you must inform us by sending an e-mail to Planner X. Please include your name, your organization name, contact information as well as the URL of your site, a list of any URLs from which you intend to link to our Website, and a list of the URLs on our site to which you would like to link. Wait 2-3 weeks for a response.<br/><br/>
+
+Approved organizations may hyperlink to our Website as follows:<br/><br/>
+
+By use of our corporate name; or<br/>
+By use of the uniform resource locator being linked to; or<br/>
+By use of any other description of our Website being linked to that makes sense within the context and format of content on the linking party’s site.<br/>
+No use of Planner X's logo or other artwork will be allowed for linking absent a trademark license agreement.<br/><br/>
+
+iFrames<br/>
+Without prior approval and written permission, you may not create frames around our Webpages that alter in any way the visual presentation or appearance of our Website.<br/><br/>
+
+Content Liability<br/>
+We shall not be hold responsible for any content that appears on your Website. You agree to protect and defend us against all claims that is rising on your Website. No link(s) should appear on any Website that may be interpreted as libelous, obscene or criminal, or which infringes, otherwise violates, or advocates the infringement or other violation of, any third party rights.<br/><br/>
+
+Your Privacy<br/>
+Please read Privacy Policy<br/><br/>
+
+Reservation of Rights<br/>
+We reserve the right to request that you remove all links or any particular link to our Website. You approve to immediately remove all links to our Website upon request. We also reserve the right to amen these terms and conditions and it’s linking policy at any time. By continuously linking to our Website, you agree to be bound to and follow these linking terms and conditions.<br/><br/>
+
+Removal of links from our website<br/>
+If you find any link on our Website that is offensive for any reason, you are free to contact and inform us any moment. We will consider requests to remove links but we are not obligated to or so or to respond to you directly.<br/><br/>
+
+We do not ensure that the information on this website is correct, we do not warrant its completeness or accuracy; nor do we promise to ensure that the website remains available or that the material on the website is kept up to date.<br/><br/>
+
+Disclaimer<br/>
+To the maximum extent permitted by applicable law, we exclude all representations, warranties and conditions relating to our website and the use of this website. Nothing in this disclaimer will:<br/><br/>
+
+limit or exclude our or your liability for death or personal injury;<br/>
+limit or exclude our or your liability for fraud or fraudulent misrepresentation;<br/>
+limit any of our or your liabilities in any way that is not permitted under applicable law; or<br/>
+exclude any of our or your liabilities that may not be excluded under applicable law.<br/>
+The limitations and prohibitions of liability set in this Section and elsewhere in this disclaimer: (a) are subject to the preceding paragraph; and (b) govern all liabilities arising under the disclaimer, including liabilities arising in contract, in tort and for breach of statutory duty.<br/>
+
+As long as the website and the information and services on the website are provided free of charge, we will not be liable for any loss or damage of any nature.</h2>
             </div>
             <Footer/>
         </div>

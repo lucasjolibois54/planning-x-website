4e4a0da278ae9a37fc4d63de96c47fdad89efffc
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-01 17:31:15 +0100

    Update usAbout.js

--- src/components/usAbout.js
+++ src/components/usAbout.js
@@ -17,11 +17,11 @@ const UsAbout = () => {
                         <h2 className="text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-5xl sm:text-6xl md:text-7xl 2xl:text-8xl font-semibold font-sans text-dark-text py-0">
                             Meet the team<br /> behind it!
                         </h2>
-                        <p className="text-xl text-light-background px-5 text-dark-text py-5 md:px-24 xl:pl-0 xl:pr-20">
+                        <p className="text-xl font-light text-light-background px-2 text-dark-text py-5 md:px-24 xl:pl-0 xl:pr-20">
                         We are a team of developers based in Copenhagen. Our team is based on 4 core values; being inspiring, hard-working, helpful, and relying. We provide free software for you to use, with no hidden fees. This is why our customers love us!
                         </p>
                         <div className="flex justify-center text-center items-center py-0 xl:justify-start">
-                            <a href="#" className="py-1 px-4 text-light-button border-2 bg-blue-button border-blue-button rounded-md mr-2 transform transition duration-300 ease-in-out hover:scale-110 hover:bg-gray-800 hover:border-gray-800">Download app</a> <a className="ml-2 py-1 px-4 text-light-button border-2 border-light-button hover:text-light-background hover:border-blue-button  transform transition duration-300 ease-in-out  hover:scale-110 rounded-md" href="#">Download for PC</a>
+                            <a href="#" className="py-1 px-4 text-light-button border-2 bg-blue-button border-blue-button rounded-md mr-2 transform transition duration-300 ease-in-out hover:scale-110 hover:bg-gray-800 hover:border-gray-800">About the software</a> <a className="ml-2 py-1 px-4 text-light-button border-2 border-light-button hover:text-light-background hover:border-blue-button  transform transition duration-300 ease-in-out  hover:scale-110 rounded-md" href="#">About the company</a>
                         </div>
                     </div>
                 </div>

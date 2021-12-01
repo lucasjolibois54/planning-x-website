9dadbe882a6105e559f2bc03287e26c0a296ed45
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-02 00:21:07 +0100

    Update softwareOverview.js

--- src/components/softwareOverview.js
+++ src/components/softwareOverview.js
@@ -16,15 +16,11 @@ const SoftwareOverview = () => {
                     </div>
                     <div className="flex text-center flex-col w-full px-3 justify-center xl:w-5/10 xl:text-left xl:pl-10">
 
-                        <h2 className="text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-5xl sm:text-6xl md:text-7xl 2xl:text-8xl font-black font-sans text-dark-text py-0">
-                            Download now <br /> for free
+                        <h2 className="text-transparent bg-clip-text bg-gradient-to-br from-light-button to-orange-text text-5xl sm:text-6xl md:text-3xl 2xl:text-3xl font-semibold font-sans text-dark-text py-0">
+                           <span className="text-blue-button">/</span>Easy access & overview
                         </h2>
-                        <p className="text-xl text-light-background px-5 text-dark-text py-5 md:px-24 xl:pl-0 xl:pr-20">
-                            Our app works on computers, tablets and phones.
-                            Just download it and register or go to your account
-                            Information is synchronized between devices.
-                            The application will tell you about itself and show
-                            the full functionality of the tooltip.
+                        <p className="text-md text-light-background font-regular px-5 text-dark-text py-5 md:px-24 xl:pl-0 xl:pr-20">
+                        Assemble all the work to be done in one place, with free acess for any given team member!
                         </p>
                         <div className="flex justify-center text-center items-center py-0 xl:justify-start">
                             <a href="#" className="py-1 px-4 text-light-button border-2 bg-blue-button border-blue-button rounded-md mr-2 transform transition duration-300 ease-in-out hover:scale-110 hover:bg-gray-800 hover:border-gray-800">Download app</a> <a className="ml-2 py-1 px-4 text-light-button border-2 border-light-button hover:text-light-background hover:border-blue-button  transform transition duration-300 ease-in-out  hover:scale-110 rounded-md" href="#">Download for PC</a>

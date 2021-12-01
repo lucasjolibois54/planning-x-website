caf2f5dc4e32daa8801ebee42f4ecf26aca29480
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-01 02:45:24 +0100

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -13,8 +13,8 @@ const Footer = () => {
     <div class="md:flex md:flex-wrap md:-mx-4 py-6 md:pb-12">
 
       <div class="footer-info lg:w-1/3 md:px-4">
-        <h4 class="text-white text-2xl mb-4">thousands of users are using Planner X and making their work life easier.</h4>
-        <p class="text-gray-400">We have carefully crafted the blocks to suit to everyone's need.</p>
+        <h4 class="text-light-button text-2xl mb-4">thousands of users are using Planner X and making their work life easier.</h4>
+        <p class="text-gray-footer">We have carefully crafted the blocks to suit to every workspace' need.</p>
         <div class="mt-4">
           {/* <button class="bg-facebook bg-blue-700 py-2 px-4 text-white rounded mt-2 transition-colors duration-300">
             <span class="fab fa-facebook-f mr-2"></span> Follow
@@ -28,30 +28,30 @@ const Footer = () => {
       <div class="md:w-2/3 lg:w-1/3 md:px-4 xl:pl-16 mt-12 lg:mt-0">
         <div class="sm:flex">
           <div class="sm:flex-1">
-            <h6 class="text-base font-medium text-white uppercase mb-2">About</h6>
+            <h6 class="text-base font-medium text-light-button uppercase mb-2">About</h6>
             <div>
-              <a href="#" class="text-gray-400 py-1 block hover:underline">Company</a>
-              <a href="#" class="text-gray-400 py-1 block hover:underline">Culture</a>
-              <a href="#" class="text-gray-400 py-1 block hover:underline">Team</a>
-              <a href="#" class="text-gray-400 py-1 block hover:underline">Software</a>
+              <a href="#" class="text-gray-footer py-1 block hover:underline">Company</a>
+              <a href="#" class="text-gray-footer py-1 block hover:underline">Culture</a>
+              <a href="#" class="text-gray-footer py-1 block hover:underline">Team</a>
+              <a href="#" class="text-gray-footer py-1 block hover:underline">Software</a>
             </div>
           </div>
           <div class="sm:flex-1 mt-4 sm:mt-0">
-            <h6 class="text-base font-medium text-white uppercase mb-2">What we offer</h6>
+            <h6 class="text-base font-medium text-light-button uppercase mb-2">What we offer</h6>
             <div>
-              <a href="#" class="text-gray-400 py-1 block hover:underline">Free software</a>
-              <a href="#" class="text-gray-400 py-1 block hover:underline">Good resources</a>
-              <a href="#" class="text-gray-400 py-1 block hover:underline">Effective Tools</a>
-              <a href="#" class="text-gray-400 py-1 block hover:underline">24/7 support</a>
-              <a href="#" class="text-gray-400 py-1 block hover:underline">Other</a>
+              <a href="#" class="text-gray-footer py-1 block hover:underline">Free software</a>
+              <a href="#" class="text-gray-footer py-1 block hover:underline">Good resources</a>
+              <a href="#" class="text-gray-footer py-1 block hover:underline">Effective Tools</a>
+              <a href="#" class="text-gray-footer py-1 block hover:underline">24/7 support</a>
+              <a href="#" class="text-gray-footer py-1 block hover:underline">Other</a>
             </div>
           </div>
         </div>
       </div>
 
       <div class="md:w-1/3 md:px-4 md:text-center mt-12 lg:mt-0">
-        <h5 class="text-lg text-white font-medium mb-4">Try the software for free</h5>
-        <button class="bg-indigo-600 text-white hover:bg-indigo-700 rounded py-2 px-6 md:px-12 transition-colors duration-300">Try it out!</button>
+        <h5 class="text-lg text-light-button font-medium mb-4">Try the software for free</h5>
+        <button class="bg-blue-button text-light-background transform transition duration-300 ease-in-out hover:scale-110 hover:bg-gray-800 hover:border-gray-800 rounded py-2 px-6 md:px-12 ">Try it out!</button>
       </div>
 
     </div>
@@ -63,11 +63,11 @@ const Footer = () => {
 
       <div class="md:flex md:-mx-4 md:items-center">
         <div class="md:flex-1 md:px-4 text-center md:text-left">
-          <p class="text-white">&copy; <strong>Planner X</strong></p>
+          <p class="text-light-button">&copy; <strong>Planner X</strong></p>
         </div>
         <div class="md:flex-1 md:px-4 text-center md:text-right">
-          <a href="#" class="py-2 px-4 text-white inline-block hover:underline">Terms of Service</a>
-          <a href="#" class="py-2 px-4 text-white inline-block hover:underline">Privacy Policy</a>
+          <a href="#" class="py-2 px-4 text-light-button inline-block hover:underline">Terms of Service</a>
+          <a href="#" class="py-2 px-4 text-light-button inline-block hover:underline">Privacy Policy</a>
         </div>
       </div>
 

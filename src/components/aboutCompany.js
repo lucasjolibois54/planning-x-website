33d500f6647309aa4262a6bd56e9aef69b333ffd
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-01 17:35:53 +0100

    Create aboutCompany.js

---
+++ src/components/aboutCompany.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import '../styles/global.css';
+
+import TheTeam from "../images/Group18.png";
+// markup
+const AboutCompany = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default AboutCompany

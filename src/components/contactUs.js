c7bdeea57f87d62d473ea252956ffd4392045afb
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-01 21:27:58 +0100

    Create contactUs.js

---
+++ src/components/contactUs.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+import '../styles/global.css';
+
+//images
+
+// markup
+const ContactUs = () => {
+    return (
+        <div className="">
+
+        </div>
+    )
+}
+
+export default ContactUs

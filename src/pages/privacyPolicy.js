f76421033acb93b1ccf30d2eab10c5b7713918f3
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-01 13:25:47 +0100

    Update privacyPolicy.js

--- src/pages/privacyPolicy.js
+++ src/pages/privacyPolicy.js
@@ -6,8 +6,62 @@ import '../styles/global.css';
 // markup
 const PravacyPolicy = () => {
     return (
-        <div className=" ">
-namnam
+        <div className="bg-maybe-dark-background w-full h-screen">
+            <div className="mx-4 text-left text-white bg-maybe-dark-background absolute top-20  md:left-1/2 transform md:-translate-x-1/2">
+                <h1 className="text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-4xl md:text-7xl text-center font-semibold ">Planner X <br /> Privacy Policy</h1>
+                <h2 className="font-light text-lg mt-10 md:w-full mb-12">
+                    This privacy policy will help you understand how Planner X uses and
+                    protects the data you provide to us when you visit and use plannerx.com.
+                    We reserve the right to change this policy at any given time, of which you will be promptly
+                    updated. If you want to make sure that you are up to date with the latest changes, we advise
+                    you to frequently visit this page.
+                    What User Data We Collect <br/><br/>
+                    When you visit the website, we may collect the following data:<br/>
+                    • Your IP address.<br/>
+                    • Your contact information and email address.<br/>
+                    • Other information such as interests and preferences.<br/>
+                    • Data profile regarding your online behavior on our website.
+                    Why We Collect Your Data<br/>
+                    We are collecting your data for several reasons:<br/>
+                    • To better understand your needs.<br/>
+                    • To improve our services and products.<br/>
+                    • To send you promotional emails containing the information we think you will find
+                    interesting.<br/>
+                    • To contact you to fill out surveys and participate in other types of market research.<br/>
+                    • To customize our website according to your online behavior and personal preferences.<br/><br/>
+                    Safeguarding and Securing the Data
+                    Planner X is committed to securing your data and keeping it confidential. Planner X has done all in its
+                    power to prevent data theft, unauthorized access, and disclosure by implementing the latest
+                    technologies and software, which help us safeguard all the information we collect online.
+                    <br/> <be/>
+                    Our Cookie Policy
+                    Once you agree to allow our website to use cookies, you also agree to use the data it collects
+                    regarding your online behavior (analyze web traffic, web pages you spend the most time on,
+                    and websites you visit).
+                    The data we collect by using cookies is used to customize our website to your needs. After we
+                    use the data for statistical analysis, the data is completely removed from our systems.
+                    Please note that cookies don't allow us to gain control of your computer in any way. They are
+                    strictly used to monitor which pages you find useful and which you do not so that we can
+                    provide a better experience for you.
+                    Terms and conditions template by WebsitePolicies.com
+                    If you want to disable cookies, you can do it by accessing the settings of your internet browser.
+                    (Provide links for cookie settings for major internet browsers).
+                    Links to Other Websites
+                    Our website contains links that lead to other websites. If you click on these links Planner X is not
+                    held responsible for your data and privacy protection. Visiting those websites is not governed by
+                    this privacy policy agreement. Make sure to read the privacy policy documentation of the
+                    website you go to from our website.
+                    Restricting the Collection of your Personal Data
+                    At some point, you might wish to restrict the use and collection of your personal data. You can
+                    achieve this by doing the following:
+                    When you are filling the forms on the website, make sure to check if there is a box which you
+                    can leave unchecked, if you don't want to disclose your personal information.
+                    If you have already agreed to share your information with us, feel free to contact us via email
+                    and we will be more than happy to change this for you.
+                    Planner X will not lease, sell or distribute your personal information to any third parties, unless we
+                    have your permission. We might do so if the law forces us. Your personal information will be
+                    used when we need to send you promotional materials if you agree to this privacy policy.</h2>
+            </div>
         </div>
     )
 }

8cf3801ebe6eab94903b6bdacdf89328118faadf
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-02 21:31:59 +0100

    Update softwareHero.js

--- src/components/softwareHero.js
+++ src/components/softwareHero.js
@@ -11,13 +11,13 @@ const SoftwareHero = () => {
     <div id="top-of-the-home-page" className="w-full  bg-center bg-no-repeat bg-cover  bg-maybe-dark-background pb-16">
         <div className="w-full h-screen flex justify-center items-center pt-28">
             <div className="mx-4 text-center text-white mt--1/3  md:left-1/2 right-1/2">
-                <h1 className="mt-20 md:mt-10 text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-4xl md:text-7xl text-center font-semibold ">How to download <br/>the app offline</h1>
+                <h1 className="mt-20 md:mt-10 text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-4xl md:text-7xl text-center font-semibold ">Get to know <br/>the software</h1>
 
                 <div>
                     <div className="flex flex-col justify-center text-center items-center py-0 pb-28">
                         
                     <div className="md:w-2/6">
-                            <h2 className="font-light text-lg mt-4 text-center sm:pr-0 mb-7">Our app works on computers, tablets and phones.Just download it and register or go to your account Information is synchronized between devices. The application will tell you about itself and show the full functionality of the tooltip.</h2>
+                            <h2 className="font-light text-lg mt-4 text-center sm:pr-0 mb-7">Our software easen your workflow and makes it easier for you to work and keeping track of the work being done through out the day art the office. Additionally it’ll also give your a clear strucutre of the tasks to be done as well as an overview.</h2>
                            </div> <div className="md:w-4/6"> <img src={SubHeroTablet2} className="pt-6" alt="mockup" />
                         </div>
                     </div>

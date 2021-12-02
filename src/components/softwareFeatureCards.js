67dfbc214f4f38e4b9969304ede39a8befc26ce9
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-02 22:03:37 +0100

    Update softwareFeatureCards.js

--- src/components/softwareFeatureCards.js
+++ src/components/softwareFeatureCards.js
@@ -5,39 +5,73 @@ import React from "react";
 function MyApp() {
   
     return (
-        
-            <div className="bg-maybe-dark-background">
-                <div className="container mx-auto">
-                    <div role="article" className="bg-maybe-dark-background py-12 md:px-8">
-                        
-                     
-
-
-
-<div class=" User centered w-full md:w-1/3">
-  <div class="bg-white rounded-lg shadow-lg">
-    <img src="https://images.unsplash.com/photo-1600054800747-be294a6a0d26?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80" alt="" class="rounded-t-lg"/>
-    <div class="p-6">
-      <h2 class="font-bold mb-2 text-2xl text-maybe-dark-background">User centered
-      </h2>
-      <p class="text-dark-background mb-2">We have created a convenient social network. Here you can communicatewith your family members, set tasks for them, propose events, plan weekends and holidays.</p>
-      <a href="#" class="text-purple-600 hover:text-purple-500 underline text-sm">Read More 👉</a>
-    </div>
-
-
     
+    
+        <div className=" bg-maybe-dark-background flex flex-col ">
+          <div className="relative m-3 flex flex-wrap mx-auto ">
+        
+            <div className="relative max-w-sm min-w-[340px] bg-white shadow-md rounded-3xl p-2 mx-1 my-3 cursor-pointer">
+              <div className="overflow-x-hidden rounded-2xl relative">
+                <img className="h-40 rounded-2xl w-full object-cover" src="https://avatars.githubusercontent.com/u/74597842?v=4"/>
+                <p className="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 group-hover:opacity-50 opacity-70" fill="none" viewBox="0 0 24 24" stroke="black">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                  </svg>
+                </p>
+              </div>
+              <div className="mt-4 pl-2 mb-2 flex justify-between ">
+                <div>
+                  <p className="text-lg font-semibold text-gray-900 mb-0">User centered</p>
+                  <p className="text-md text-gray-800 mt-0">We have created a convenient social network. Here you can communicatewith your family members, set tasks for them, propose events, plan weekends and holidays.</p>
+                </div>
+              </div>
+            </div>
+        
 
-  </div>
-</div>
+            <div className="relative max-w-sm min-w-[340px] bg-white shadow-md rounded-3xl p-2 mx-1 my-3 cursor-pointer">
+              <div className="overflow-x-hidden rounded-2xl relative">
+                <img className="h-40 rounded-2xl w-full object-cover" src="https://avatars.githubusercontent.com/u/74597842?v=4"/>
+                <p className="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 group-hover:opacity-50 opacity-70" fill="none" viewBox="0 0 24 24" stroke="black">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                  </svg>
+                </p>
+              </div>
+              <div className="mt-4 pl-2 mb-2 flex justify-between ">
+                <div>
+                  <p className="text-lg font-semibold text-gray-900 mb-0">User centered</p>
+                  <p className="text-md text-gray-800 mt-0">We have created a convenient social network. Here you can communicatewith your family members, set tasks for them, propose events, plan weekends and holidays.</p>
+                </div>
+              </div>
+            </div>
+        
+
+
+            <div className="relative max-w-sm min-w-[340px] bg-white shadow-md rounded-3xl p-2 mx-1 my-3 cursor-pointer">
+              <div className="overflow-x-hidden rounded-2xl relative">
+                <img className="h-40 rounded-2xl w-full object-cover" src="https://avatars.githubusercontent.com/u/74597842?v=4"/>
+                <p className="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 group-hover:opacity-50 opacity-70" fill="none" viewBox="0 0 24 24" stroke="black">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                  </svg>
+                </p>
+              </div>
+              <div className="mt-4 pl-2 mb-2 flex justify-between ">
+                <div>
+                  <p className="text-lg font-semibold text-gray-900 mb-0">User centered</p>
+                  <p className="text-md text-gray-800 mt-0">We have created a convenient social network. Here you can communicatewith your family members, set tasks for them, propose events, plan weekends and holidays.</p>
+                </div>
+              </div>
+            </div>
+        
+          </div>
+        </div>
 
 
 
 
                                 
-                            </div>
-                        </div>
-                    </div>
-              
+   
         
     );
 }

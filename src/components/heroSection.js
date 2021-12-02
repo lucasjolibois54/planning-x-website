41d66e0dbd094069b20e4db19de65612d48e9362
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-02 05:02:36 +0100

    Update heroSection.js

--- src/components/heroSection.js
+++ src/components/heroSection.js
@@ -8,8 +8,8 @@ const HeroSection = () => {
       <div>
           <div className="w-full h-100v bg-center bg-no-repeat bg-cover  bg-maybe-dark-background pb-16">
     <div className="w-full h-screen flex justify-center items-center">
-        <div className="mx-4 text-center text-white absolute top-1/2  md:left-1/2 transform md:-translate-x-1/2 -translate-y-1/2">
-        <h1 className="text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-4xl md:text-7xl text-center font-semibold ">Your digital <br/> daily reminder</h1>
+        <div className="mx-4 md:mt-10 text-center text-white absolute top-1/2  md:left-1/2 transform md:-translate-x-1/2 -translate-y-1/2">
+        <h1 className=" text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-4xl md:text-7xl text-center font-semibold ">Your digital <br/> daily reminder</h1>
             <h2 className="font-light text-lg mt-10 md:ml-20 md:w-96 md:mr-20 sm:pr-0 mb-12">Planner X is a revolutionary, free to use highly innovative software solution helping your work team traking their tasks and roles! Using our system, you'll be able to distribute work tasks/ assignments to your team through the app. The members of your team will later on be able to mark the task when it's done. It's simple, and it is free! What's not to like?</h2>
             <div>
                 {/* <a href="" class="bg-blue-500 rounded-md font-bold text-white text-center px-4 py-3 transition duration-300 ease-in-out hover:bg-blue-600 mr-2">

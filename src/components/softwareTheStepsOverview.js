48ccaf3c56ef9d3561f9e2725dbd09088e58ad3d
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-02 19:10:40 +0100

    Create softwareTheStepsOverview.js

---
+++ src/components/softwareTheStepsOverview.js
@@ -0,0 +1,45 @@
+import * as React from "react"
+import '../styles/global.css';
+
+
+// markup
+const SoftwareTheStepsOverview = () => {
+    return (
+        <div>
+            <div className=" bg-maybe-dark-background w-full h-screen pt-10 lg:pt-0 pb-20">
+
+                <div  id="tips-about-page" className="mb-28 p-0 flex flex-col-reverse w-full xl:flex-row-reverse md:p-16 md:mb-18 ">
+                    <div className="w-full xl:w-9/12">
+                    <h2 className="text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-4xl sm:text-4xl md:text-6xl 2xl:text-8xl font-bold text-dark-text py-0">
+                    {" "}
+                        </h2>
+                    <div className="text-lg text-light-background px-5 text-dark-text py-5 md:px-24 xl:pl-0 xl:pr-20">
+                            <ul>
+                                <li className="list-none"><p className="font-light text-center md:text-left px-5 py-1 rounded-xl md:inline-block bg-dark-background">Go on to our website</p></li><br/>
+                                <li className="list-none"><p className="font-light text-center md:text-left md:px-5 py-1 rounded-xl md:inline-block bg-dark-background">Click on the top right corner of the serach bar on the icon </p></li><br/>
+                                <li className="list-none"><p className="font-light text-center md:text-left md:px-5 py-1 rounded-xl md:inline-block bg-dark-background">Now youre ready to go offline </p></li> <br/>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="flex text-center flex-col w-full px-3 justify-center xl:w-5/10 xl:text-left xl:pl-10">
+                    <h2 className="text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-4xl sm:text-4xl md:text-6xl 2xl:text-8xl font-bold text-dark-text py-0">
+                    The steps
+                        </h2>
+ 
+                        <div className="text-lg text-light-background px-5 text-dark-text py-5 md:px-24 xl:pl-0 xl:pr-20">
+                            <ul>
+                                <li className="list-disc"><p className="font-light text-left">The application is a SPA SaaS web application and it easen your workflow with your team. Through the app you can distribute tasks while also adding a timeframe & more.</p></li><br/>
+                                <li className="list-disc"><p className="font-light text-left">Through Planner X, your coworkers will be able to mark their tasks as done when they have finished it, which will give you a clear overview of what has been done throughout the day.</p></li>
+                            </ul>
+                        </div>
+                        {/* <div className="flex justify-center text-center items-center py-0 xl:justify-start">
+                            <a href="#" className="py-1 px-4 text-light-button border-2 bg-blue-button border-blue-button rounded-md mr-2 transform transition duration-300 ease-in-out hover:scale-110 hover:bg-gray-800 hover:border-gray-800">Download app</a> <a className="ml-2 py-1 px-4 text-light-button border-2 border-light-button hover:text-light-background hover:border-blue-button  transform transition duration-300 ease-in-out  hover:scale-110 rounded-md" href="#">Download for PC</a>
+                        </div> */}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SoftwareTheStepsOverview

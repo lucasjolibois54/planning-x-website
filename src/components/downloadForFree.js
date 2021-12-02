2faeac58c209d3bcc87705f6570322be5a3ae179
Marc140700 <74597768+Marc140700@users.noreply.github.com>
2021-12-02 16:17:28 +0100

    Update downloadForFree.js

--- src/components/downloadForFree.js
+++ src/components/downloadForFree.js
@@ -18,11 +18,12 @@ const DownloadForFree = () => {
                             Download now <br /> for free
                         </h2>
                         <p className="text-xl text-light-background px-5 text-dark-text py-5 md:px-24 xl:pl-0 xl:pr-20">
-                            Our app works on computers, tablets and phones.
-                            Just download it and register or go to your account
-                            Information is synchronized between devices.
-                            The application will tell you about itself and show
-                            the full functionality of the tooltip.
+                            Our app is available on all devices.
+                            You can download it on Appstore and Google Play,
+                            or even directly through our website. This works
+                            for both computer and smaller devices. You can
+                            also use our software through the browser
+                             - even offline!
                         </p>
                         <div className="flex justify-center text-center items-center py-0 xl:justify-start">
                             <a href="#" className="py-1 px-4 text-light-button border-2 bg-blue-button border-blue-button rounded-md mr-2 transform transition duration-300 ease-in-out hover:scale-110 hover:bg-gray-800 hover:border-gray-800">Download app</a> <a className="ml-2 py-1 px-4 text-light-button border-2 border-light-button hover:text-light-background hover:border-blue-button  transform transition duration-300 ease-in-out  hover:scale-110 rounded-md" href="#">Download for PC</a>

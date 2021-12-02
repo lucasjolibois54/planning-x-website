93d3708952251294cfd9a4e2fd9d057ae8fe1c74
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-02 01:12:43 +0100

    Update featuredSection.js

--- src/components/featuredSection.js
+++ src/components/featuredSection.js
@@ -19,7 +19,7 @@ const FeaturedSection = () => {
                         Featured to help you with time planning
                     </h2>
                     <p className="text-xl text-light-background px-5 text-dark-text py-5 md:px-24 xl:pl-0 xl:pr-20">
-                        We have created a convenient social network. Here you can communicatewith your family members, set tasks for them, propose events, plan weekends and holidays. It will be easier for you to see which family member is free tomorrow and will be able to cook dinner. Children will find it easier to plan their time when all the tasks are ahead of them. And it will be easier for you to control and help them.
+                        We have created a convenient network for work management. Here you can communicate with your team members, set tasks for them, propose events, plans & more. It will be easier for you to see which coworkers are working on a task. Your team will find it easier to plan their time when all the tasks are ahead of them. And it will be easier for you to control and help them.
                     </p>
                     <div className="flex justify-center text-center items-center py-0 xl:justify-start">
                         <a href="https://plannerx.netlify.app/" className="py-1 px-4 text-light-button border-2 bg-blue-button border-blue-button rounded-md mr-2 transform transition duration-300 ease-in-out hover:scale-110 hover:bg-gray-800 hover:border-gray-800">Download app</a> <a className="ml-2 py-1 px-4 text-light-button border-2 border-light-button hover:text-light-background hover:border-blue-button  transform transition duration-300 ease-in-out  hover:scale-110 rounded-md" href="https://plannerx.netlify.app/">Download for PC</a>

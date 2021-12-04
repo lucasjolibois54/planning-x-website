f2d819942b160beb6f19a72542d609ba1d6395be
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-04 23:54:18 +0100

    Update subHero.js

--- src/components/subHero.js
+++ src/components/subHero.js
@@ -9,13 +9,13 @@ import SubHeroTablet from "../images/Rectangle276(1).png";
 const SubHero = () => {
       return (
             <div className="-mt-5 lg:-mt-1">
-                  <div className="h-20v md:h-0v bg-maybe-dark-background"></div>
+                  <div className="md:h-7v h-20v bg-maybe-dark-background "></div>
                   <div className="relative">
                         <div className="w-full xl:w-7/12 absolute  left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                               <img src={SubHeroTablet} className="pt-6" alt="mockup" />
                         </div>
                   </div>
-                  <div className="h-30v md:h-50v bg-light-background"></div>
+                  <div className="h-20v md:h-50v bg-light-background"></div>
             </div>
       )
 }

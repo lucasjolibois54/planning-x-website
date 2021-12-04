b7f44e1f92f2e055b26c6ae9a3f95e5b2314e3d2
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-04 20:57:42 +0100

    Update softwareOverview.js

--- src/components/softwareOverview.js
+++ src/components/softwareOverview.js
@@ -10,30 +10,30 @@ const SoftwareOverview = () => {
     <div>
             <div className=" bg-maybe-dark-background w-full h-screen pt-24 lg:pt-0 pb-16">
 
-                <div id="software-overview-on-software-page" className="mb-18 p-0 flex flex-col-reverse w-full xl:flex-row-reverse md:p-16 md:mb-18 ">
+                <div id="software-overview-on-software-page" className="mb-18 p-0 flex flex-col-reverse w-full lg:flex-row-reverse md:p-16 md:mb-18 ">
                     <div className="w-full xl:w-9/12">
                         <img src={devicesSotware} className="pt-6" alt="mockup" />
                     </div>
-                    <div className="flex text-center flex-col w-full px-3 justify-center xl:w-5/10 xl:text-left xl:pl-10">
+                    <div className="flex text-center flex-col w-full px-3 justify-center xl:w-5/10 lg:text-left xl:pl-10">
 
                         <h2 className="text-transparent bg-clip-text bg-gradient-to-br from-light-button to-orange-text text-2xl sm:text-3xl md:text-3xl 2xl:text-3xl font-semibold font-sans text-dark-text py-0">
                            <span className="text-blue-button">/</span>{" "}Easy access & overview
                         </h2>
-                        <p className="text-md text-light-background font-regular px-5 text-dark-text py-5 md:px-24 xl:pl-0 xl:pr-20">
+                        <p className="text-md text-light-background font-regular px-5 text-dark-text py-5 xl:px-24 xl:pl-0 xl:pr-20">
                         Assemble all the work to be done in one place, with free acess for any given team member!
                         </p>
                         
                         <h2 className="text-transparent bg-clip-text bg-gradient-to-br from-light-button to-orange-text text-2xl sm:text-3xl md:text-3xl 2xl:text-3xl font-semibold font-sans text-dark-text py-0">
                            <span className="text-blue-button">/</span>{" "}Tidy up your teamwork
                         </h2>
-                        <p className="text-md text-light-background font-regular px-5 text-dark-text py-5 md:px-24 xl:pl-0 xl:pr-20">
+                        <p className="text-md text-light-background font-regular px-5 text-dark-text py-5 xl:px-24 xl:pl-0 xl:pr-20">
                         You don’t need to worry about your team not knowing what to do!
                         </p>
 
                         <h2 className="text-transparent bg-clip-text bg-gradient-to-br from-light-button to-orange-text text-2xl sm:text-3xl md:text-3xl 2xl:text-3xl font-semibold font-sans text-dark-text py-0">
                            <span className="text-blue-button">/</span>{" "} Collaboration meets knowledge
                         </h2>
-                        <p className="text-md text-light-background font-regular px-5 text-dark-text py-5 md:px-24 xl:pl-0 xl:pr-20">
+                        <p className="text-md text-light-background font-regular px-5 text-dark-text py-5 xl:px-24 xl:pl-0 xl:pr-20">
                         Distribute assignments to your coworkers no matter when or where your are!
                         </p>
                     </div>

db66f99160b0c657c015333e28e2170608bf416c
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-05 00:01:11 +0100

    Update softwareTheStepsOverview.js

--- src/components/softwareTheStepsOverview.js
+++ src/components/softwareTheStepsOverview.js
@@ -22,7 +22,7 @@ const SoftwareTheStepsOverview = () => {
                         </div>
                     </div>
                     <div className="flex text-center flex-col w-full px-3 justify-center xl:w-5/10 xl:text-left xl:pl-10">
-                    <h2 className="text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-4xl sm:text-4xl md:text-6xl 2xl:text-8xl font-bold text-dark-text py-0">
+                    <h2 className="text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-4xl sm:text-4xl md:text-6xl 2xl:text-8xl font-semibold text-dark-text py-0">
                     The steps
                         </h2>
  

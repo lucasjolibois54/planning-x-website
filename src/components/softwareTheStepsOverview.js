c5117956e02c7a6addcd18f696c2d824e6140684
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-04 21:26:10 +0100

    Update softwareTheStepsOverview.js

--- src/components/softwareTheStepsOverview.js
+++ src/components/softwareTheStepsOverview.js
@@ -8,13 +8,13 @@ const SoftwareTheStepsOverview = () => {
         <div>
             <div className=" bg-maybe-dark-background w-full h-screen pt-0 lg:pt-0 pb-5">
 
-                <div  id="tips-about-page" className="mb-3 p-0 flex flex-col-reverse w-full xl:flex-row-reverse md:p-16 md:mb-7 ">
+                <div  id="tips-about-page" className="mb-3 p-0 flex flex-col-reverse w-full lg:flex-row-reverse md:p-16 md:mb-7 ">
                     <div className="w-full xl:w-9/12">
                     <h2 className="text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-4xl sm:text-4xl md:text-6xl 2xl:text-8xl font-bold text-dark-text py-0">
                     {" "}
                         </h2>
                     <div className="text-lg text-light-background px-5 text-dark-text py-5 md:px-24 xl:pl-0 xl:pr-20">
-                            <ul>
+                            <ul className="my-auto ">
                                 <li className="list-none"><p className="font-light text-center md:text-left px-5 py-1 rounded-xl md:inline-block bg-dark-background">Go on to our website</p></li><br/>
                                 <li className="list-none"><p className="font-light text-center md:text-left md:px-5 py-1 rounded-xl md:inline-block bg-dark-background">Click on the top right corner of the serach bar on the icon </p></li><br/>
                                 <li className="list-none"><p className="font-light text-center md:text-left md:px-5 py-1 rounded-xl md:inline-block bg-dark-background">Now youre ready to go offline </p></li> <br/>
@@ -26,7 +26,7 @@ const SoftwareTheStepsOverview = () => {
                     The steps
                         </h2>
  
-                        <div className="text-lg text-light-background px-5 text-dark-text py-5 md:px-24 xl:pl-0 xl:pr-20">
+                        <div className="text-lg text-light-background px-5 text-dark-text py-5 md:px-24 lg:px-0 xl:px-24 xl:pl-0 xl:pr-20">
                             <ul>
                                 <li className="list-disc"><p className="font-light text-left">Go on to our website, and navigate to a download button. You'll get the opportunity to use Planner X directly through the website, or download it on any device you want.</p></li><br/>
                                 <li className="list-disc"><p className="font-light text-left">Play around with it, and try adding different tasks with text and the colors you want. Use it either online or offline - the choice is yours!</p></li>

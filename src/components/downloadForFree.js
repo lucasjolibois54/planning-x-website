7f5f2734cb3b9e338957e5e0ba75a8fe1e7d7b8d
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-04 23:20:21 +0100

    Update downloadForFree.js

--- src/components/downloadForFree.js
+++ src/components/downloadForFree.js
@@ -6,18 +6,18 @@ import Image88 from "../images/image88.png";
 const DownloadForFree = () => {
     return (
         <div>
-            <div className=" bg-maybe-dark-background w-full h-screen pt-24 lg:pt-0 pb-20">
+            <div className=" bg-maybe-dark-background w-full h-screen md:pt-24 lg:pt-0 pb-20">
 
-                <div id="about" className="mb-28 p-0 flex flex-col-reverse w-full lg:flex-row-reverse md:p-16 md:mb-18 ">
+                <div id="about" className="md:mb-28 p-0 flex flex-col-reverse w-full lg:flex-row-reverse md:p-16 md:mb-18 ">
                     <div className="w-full lg:my-auto lg:w-9/12">
                         <img src={Image88} className="pt-6" alt="mockup" />
                     </div>
                     <div className="flex text-center flex-col w-full px-3 justify-center lg:w-5/10 lg:text-left xl:pl-10">
 
-                        <h2 className="text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-5xl sm:text-6xl md:text-7xl lg:text-6xl 2xl:text-8xl font-bold text-dark-text py-0">
+                        <h2 className="text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-4xl sm:text-5xl md:text-7xl lg:text-6xl 2xl:text-8xl font-bold text-dark-text py-0">
                             Download now <br /> for free
                         </h2>
-                        <p className="text-xl text-light-background px-5 text-dark-text py-5 md:px-24 lg:pl-0 xl:pr-20">
+                        <p className="text-lg text-light-background px-5 text-dark-text py-5 md:px-24 lg:pl-0 xl:pr-20">
                             Our app is available on all devices.
                             You can download it on Appstore and Google Play,
                             or even directly through our website. This works

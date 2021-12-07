f76cc2b508e95c532d5c5e8b06b849d99171a786
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-07 02:39:51 +0100

    Update downloadForFree.js

--- src/components/downloadForFree.js
+++ src/components/downloadForFree.js
@@ -19,20 +19,22 @@ const DownloadForFree = () => {
                         <img  data-aos="fade-up" src={Image88} className="pt-6" alt="mockup" />
                     </div>
                     <div className="flex text-center flex-col w-full px-3 justify-center lg:w-5/10 lg:text-left xl:pl-10">
-
-                        <h2  data-aos="fade-up" className="text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-4xl sm:text-5xl md:text-7xl lg:text-6xl 2xl:text-8xl font-bold text-dark-text py-0">
-                            Download now <br /> for free
-                        </h2>
-                        <p  data-aos="fade-up" className="text-lg text-light-background px-5 text-dark-text py-5 md:px-24 lg:pl-0 xl:pr-20">
+                    <div data-aos="fade-up">
+                        <h2 className="text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-4xl sm:text-5xl md:text-7xl lg:text-6xl 2xl:text-8xl font-bold text-dark-text py-0">
+                           Download now <br /> for free
+                        </h2></div>
+                        <p className="text-lg text-light-background px-5 text-dark-text py-5 md:px-24 lg:pl-0 xl:pr-20">
+                        <div data-aos="fade-up">
                             Our app is available on all devices.
                             You can download it on Appstore and Google Play,
                             or even directly through our website. This works
                             for both computer and smaller devices. You can
                             also use our software through the browser
                              - even offline!
+                             </div>
                         </p>
                         <div className="flex justify-center text-center items-center py-0 lg:justify-start">
-                            <a href="#" className="py-1 px-4 text-light-button border-2 bg-blue-button border-blue-button rounded-md mr-2 transform transition duration-300 ease-in-out hover:scale-110 hover:bg-gray-800 hover:border-gray-800">Download app</a> <a className="ml-2 py-1 px-4 text-light-button border-2 border-light-button hover:text-light-background hover:border-blue-button  transform transition duration-300 ease-in-out  hover:scale-110 rounded-md" href="#">Download for PC</a>
+                            <a data-aos="fade-up" href="#" className="py-1 px-4 text-light-button border-2 bg-blue-button border-blue-button rounded-md mr-2 transform transition duration-300 ease-in-out hover:scale-110 hover:bg-gray-800 hover:border-gray-800">Download app</a> <a data-aos="fade-up" className="ml-2 py-1 px-4 text-light-button border-2 border-light-button hover:text-light-background hover:border-blue-button  transform transition duration-300 ease-in-out  hover:scale-110 rounded-md" href="#">Download for PC</a>
                         </div>
                     </div>
                 </div>

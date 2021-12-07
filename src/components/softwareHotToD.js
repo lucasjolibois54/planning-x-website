3f616151854f1b0ef8fd33ca3aeda57c5e5591c5
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-07 02:21:49 +0100

    Update softwareHotToD.js

--- src/components/softwareHotToD.js
+++ src/components/softwareHotToD.js
@@ -8,12 +8,15 @@ import { useEffect } from 'react';
 import Steps5 from "../images/download-steps-one.svg";
 // markup
 const SoftwareHotToD = () => {
+    useEffect(() => {
+        Aos.init({ duration: 2000 });
+    }, []);
     return (
         <div>
             <div id="install-guide-section" className="w-full  bg-center bg-no-repeat bg-cover  bg-maybe-dark-background pb-16">
                 <div className="w-full h-screen flex justify-center items-center pt-28">
                     <div className="mx-4 text-center text-white mt--1/3  md:left-1/2 right-1/2">
-                        <h1 className="md:mt-10 text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-4xl md:text-7xl text-center font-semibold ">How to download <br/>the app offline</h1>
+                        <h1 data-aos="fade-up" className="md:mt-10 text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-4xl md:text-7xl text-center font-semibold ">How to download <br/>the app offline</h1>
 
                         <div>
                             <div className="flex justify-center text-center items-center py-0 pb-20">

571cafeda3248de33fbb36d541e88a81f812b7e1
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-07 02:19:49 +0100

    Update downloadForFree.js

--- src/components/downloadForFree.js
+++ src/components/downloadForFree.js
@@ -1,23 +1,29 @@
 import * as React from "react"
 import '../styles/global.css';
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from 'react';
 
 import Image88 from "../images/image88.png";
 // markup
 const DownloadForFree = () => {
+    useEffect(() => {
+        Aos.init({ duration: 2000 });
+    }, []);
     return (
         <div>
             <div className=" bg-maybe-dark-background w-full h-screen md:pt-24 lg:pt-0 pb-20">
 
                 <div id="about" className="md:mb-28 p-0 flex flex-col-reverse w-full lg:flex-row-reverse md:p-16 md:mb-18 ">
                     <div className="w-full lg:my-auto lg:w-9/12">
-                        <img src={Image88} className="pt-6" alt="mockup" />
+                        <img  data-aos="fade-up" src={Image88} className="pt-6" alt="mockup" />
                     </div>
                     <div className="flex text-center flex-col w-full px-3 justify-center lg:w-5/10 lg:text-left xl:pl-10">
 
-                        <h2 className="text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-4xl sm:text-5xl md:text-7xl lg:text-6xl 2xl:text-8xl font-bold text-dark-text py-0">
+                        <h2  data-aos="fade-up" className="text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-4xl sm:text-5xl md:text-7xl lg:text-6xl 2xl:text-8xl font-bold text-dark-text py-0">
                             Download now <br /> for free
                         </h2>
-                        <p className="text-lg text-light-background px-5 text-dark-text py-5 md:px-24 lg:pl-0 xl:pr-20">
+                        <p  data-aos="fade-up" className="text-lg text-light-background px-5 text-dark-text py-5 md:px-24 lg:pl-0 xl:pr-20">
                             Our app is available on all devices.
                             You can download it on Appstore and Google Play,
                             or even directly through our website. This works

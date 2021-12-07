964ef1162c8a6581a54e2944f6d4ffe21dd929f6
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-07 02:19:27 +0100

    Update softwareTheStepsOverview.js

--- src/components/softwareTheStepsOverview.js
+++ src/components/softwareTheStepsOverview.js
@@ -1,9 +1,14 @@
 import * as React from "react"
 import '../styles/global.css';
-
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from 'react';
 
 // markup
 const SoftwareTheStepsOverview = () => {
+    useEffect(() => {
+        Aos.init({ duration: 2000 });
+    }, []);
     return (
         <div>
             <div className=" bg-maybe-dark-background w-full h-screen pt-0 lg:pt-0 pb-5">
@@ -15,21 +20,21 @@ const SoftwareTheStepsOverview = () => {
                         </h2>
                     <div className="text-lg text-light-background px-5 text-dark-text py-5 md:px-24 xl:pl-0 xl:pr-20">
                             <ul className="my-auto ">
-                                <li className="list-none"><p className="font-light text-center md:text-left px-5 py-1 rounded-xl md:inline-block bg-dark-background">Go on to our website</p></li><br/>
-                                <li className="list-none"><p className="font-light text-center md:text-left md:px-5 py-1 rounded-xl md:inline-block bg-dark-background">Click on the top right corner of the serach bar on the icon </p></li><br/>
-                                <li className="list-none"><p className="font-light text-center md:text-left md:px-5 py-1 rounded-xl md:inline-block bg-dark-background">Now youre ready to go offline </p></li> <br/>
+                                <li data-aos="fade-up" className="list-none"><p className="font-light text-center md:text-left px-5 py-1 rounded-xl md:inline-block bg-dark-background">Go on to our website</p></li><br/>
+                                <li data-aos="fade-up" className="list-none"><p className="font-light text-center md:text-left md:px-5 py-1 rounded-xl md:inline-block bg-dark-background">Click on the top right corner of the serach bar on the icon </p></li><br/>
+                                <li data-aos="fade-up" className="list-none"><p className="font-light text-center md:text-left md:px-5 py-1 rounded-xl md:inline-block bg-dark-background">Now youre ready to go offline </p></li> <br/>
                             </ul>
                         </div>
                     </div>
                     <div className="flex text-center flex-col w-full px-3 justify-center xl:w-5/10 xl:text-left xl:pl-10">
-                    <h2 className="text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-4xl sm:text-4xl md:text-6xl 2xl:text-8xl font-semibold text-dark-text py-0">
+                    <h2 data-aos="fade-up" className="text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-4xl sm:text-4xl md:text-6xl 2xl:text-8xl font-semibold text-dark-text py-0">
                     The steps
                         </h2>
  
                         <div className="text-lg text-light-background px-5 text-dark-text py-5 md:px-24 lg:px-0 xl:px-24 xl:pl-0 xl:pr-20">
                             <ul>
-                                <li className="list-disc"><p className="font-light text-left">Go on to our website, and navigate to a download button. You'll get the opportunity to use Planner X directly through the website, or download it on any device you want.</p></li><br/>
-                                <li className="list-disc"><p className="font-light text-left">Play around with it, and try adding different tasks with text and the colors you want. Use it either online or offline - the choice is yours!</p></li>
+                                <li data-aos="fade-up" className="list-disc"><p className="font-light text-left">Go on to our website, and navigate to a download button. You'll get the opportunity to use Planner X directly through the website, or download it on any device you want.</p></li><br/>
+                                <li data-aos="fade-up" className="list-disc"><p className="font-light text-left">Play around with it, and try adding different tasks with text and the colors you want. Use it either online or offline - the choice is yours!</p></li>
                             </ul>
                         </div>
                         {/* <div className="flex justify-center text-center items-center py-0 xl:justify-start">

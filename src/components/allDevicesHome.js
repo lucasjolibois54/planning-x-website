63688de59c23737e37018a4b3556e26c498e5a52
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-19 18:05:09 +0100

    update

--- src/components/allDevicesHome.js
+++ src/components/allDevicesHome.js
@@ -20,12 +20,13 @@ const AllDevicesHome = () => {
                 <div className="w-full lg:w-full">
                     <img  data-aos="flip-down" src={DevicesImg} className="pt-6" alt="mockup" />
                 </div>
-                <div className="flex text-center flex-col w-full px-3 justify-center xl:w-5/10 lg:text-left lg:pl-10">
+                <div  className="flex text-center flex-col w-full px-3 justify-center xl:w-5/10 lg:text-left lg:pl-10">
 
                     <h2 data-aos="fade-up" className="text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-5xl sm:text-6xl md:text-7xl 2xl:text-8xl font-bold text-dark-text py-0">
                         Works on all devices
                     </h2>
-                    <p  data-aos="fade-up" className="text-xl px-5 text-dark-text py-5 md:px-24 lg:pl-0 xl:pr-20">
+                    <div data-aos="fade-up" >
+                    <p  className="text-xl px-5 text-dark-text py-5 md:px-24 lg:pl-0 xl:pr-20">
                         Our app works on computers, tablets and phones.
                         Just download it and register or go to your account
                         Information is synchronized between devices.
@@ -33,7 +34,8 @@ const AllDevicesHome = () => {
                         the full functionality of the tooltip.
                     </p>
                     <div className="flex justify-center text-center items-center py-0 lg:justify-start">
-                        <a  data-aos="fade-up" className=" cursor-pointer py-1 px-4 text-light-button border-2 bg-black-button border-black-button rounded-md mr-2 transform transition duration-300 ease-in-out hover:scale-110 hover:bg-gray-800 hover:border-gray-800"  onClick={()=>{navigate("/software#install-guide-section")}}>Install guide</a> <a  data-aos="fade-up" className="cursor-pointer ml-2 py-1 px-4 text-black-button border-2 border-black-button hover:border-blue-button transform transition duration-300 ease-in-out  hover:scale-110 rounded-md"  onClick={()=>{navigate("/aboutUs")}}>About us</a>
+                        <a  className=" cursor-pointer py-1 px-4 text-light-button border-2 bg-black-button border-black-button rounded-md mr-2 transform transition duration-300 ease-in-out hover:scale-110 hover:bg-gray-800 hover:border-gray-800"  onClick={()=>{navigate("/software#install-guide-section")}}>Install guide</a> <a  className="cursor-pointer ml-2 py-1 px-4 text-black-button border-2 border-black-button hover:border-blue-button transform transition duration-300 ease-in-out  hover:scale-110 rounded-md"  onClick={()=>{navigate("/aboutUs")}}>About us</a>
+                    </div>
                     </div>
                 </div>
             </div>
--- src/components/downloadForFree.js
+++ src/components/downloadForFree.js
@@ -23,8 +23,9 @@ const DownloadForFree = () => {
                         <h2 className="text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-4xl sm:text-5xl md:text-7xl lg:text-6xl 2xl:text-8xl font-bold text-dark-text py-0">
                            Download now <br /> for free
                         </h2></div>
-                        <p className="text-lg text-light-background px-5 text-dark-text py-5 md:px-24 lg:pl-0 xl:pr-20">
                         <div data-aos="fade-up">
+                        <p className="text-lg text-light-background px-5 text-dark-text py-5 md:px-24 lg:pl-0 xl:pr-20">
+                        <div>
                             Our app is available on all devices.
                             You can download it on Appstore and Google Play,
                             or even directly through our website. This works
@@ -34,7 +35,8 @@ const DownloadForFree = () => {
                              </div>
                         </p>
                         <div className="flex justify-center text-center items-center py-0 lg:justify-start">
-                            <a data-aos="fade-up" href="https://plannerx-software.netlify.app/" className="py-1 px-4 text-light-button border-2 bg-blue-button border-blue-button rounded-md mr-2 transform transition duration-300 ease-in-out hover:scale-110 hover:bg-gray-800 hover:border-gray-800">Download app</a> <a data-aos="fade-up" className="ml-2 py-1 px-4 text-light-button border-2 border-light-button hover:text-light-background hover:border-blue-button  transform transition duration-300 ease-in-out  hover:scale-110 rounded-md" href="https://plannerx-software.netlify.app/">Download for PC</a>
+                            <a  href="https://plannerx-software.netlify.app/" className="py-1 px-4 text-light-button border-2 bg-blue-button border-blue-button rounded-md mr-2 transform transition duration-300 ease-in-out hover:scale-110 hover:bg-gray-800 hover:border-gray-800">Download app</a> <a className="ml-2 py-1 px-4 text-light-button border-2 border-light-button hover:text-light-background hover:border-blue-button  transform transition duration-300 ease-in-out  hover:scale-110 rounded-md" href="https://plannerx-software.netlify.app/">Download for PC</a>
+                        </div>
                         </div>
                     </div>
                 </div>

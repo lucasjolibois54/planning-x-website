f4c3b79f351bafe47c16b21cce610fa0d647ddea
lucasjolibois54 <lucasjolibois@icloud.com>
2021-12-19 15:37:53 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,22 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://planner-x.netlify.app/",
     title: "Planning X",
   },
-  plugins: ["gatsby-plugin-styled-components", 'gatsby-plugin-postcss'],
-};
+
+  plugins: [      "gatsby-plugin-styled-components", 'gatsby-plugin-postcss',
+      {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'React go',
+        short_name: 'Reactgo',
+        start_url: '/',
+        background_color: '#f7f0eb',
+        theme_color: '#a2466c',
+        display: 'standalone',
+        icon: 'src/images/pro-icon.png',
+      },
+    }
+
+  ],
+}

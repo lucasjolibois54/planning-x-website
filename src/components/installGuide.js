b2800aa854c92e1aa6ff9ef55343d78f2f399fda
Daudmir <74597831+Daudmir@users.noreply.github.com>
2021-12-19 17:49:20 +0100

    Update installGuide.js

--- src/components/installGuide.js
+++ src/components/installGuide.js
@@ -3,6 +3,7 @@ import '../styles/global.css';
 import Aos from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from 'react';
+import { navigate } from "gatsby"
 
 import DownloadSteps from "../images/how-to-4.png";
 // markup
@@ -20,14 +21,14 @@ const InstallGuide = () => {
                     </div>
                     <div data-aos="fade-up" data-aos-anchor-placement="top-center" className="md:mr-7 flex text-center flex-col w-full px-3 justify-center lg:w-5/10 lg:text-left xl:pl-10">
 
-                        <h2  className="lg:text-right  md:mr-7 px-2 text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-5xl sm:text-5xl md:text-6xl lg:text-7xl 2xl:text-7xl font-semibold text-dark-text py-0">
-                        How to download
+                        <h2 className="lg:text-right  md:mr-7 px-2 text-transparent bg-clip-text bg-gradient-to-br from-purple-text to-orange-text text-5xl sm:text-5xl md:text-6xl lg:text-7xl 2xl:text-7xl font-semibold text-dark-text py-0">
+                            How to download
                         </h2>
-                        <p  className="text-xl font-light text-light-background px-2 text-dark-text py-5 md:mr-7 xl:pl-0  lg:text-right">
-                        On this page there is a search bar where you can see our <a className="color-purple-text" href="#"> websites </a> address. On the right side of the bar there is some icons that you can click on. Press on the icon that is illustrated and shown on the right side
+                        <p className="text-xl font-light text-light-background px-2 text-dark-text py-5 md:mr-7 xl:pl-0  lg:text-right">
+                            On this page there is a search bar where you can see our <a className="color-purple-text" href="#"> websites </a> address. On the right side of the bar there is some icons that you can click on. Press on the icon that is illustrated and shown on the right side
                         </p>
                         <div className="flex justify-center text-center items-center py-0 px-2 lg:justify-end md:mr-7 ">
-                            <a  href="#" className="py-1 px-4 text-light-button border-2 bg-blue-button border-blue-button rounded-md mr-2 transform transition duration-300 ease-in-out hover:scale-110 hover:bg-gray-800 hover:border-gray-800">About the software</a> <a  className="ml-2 py-1 px-4 text-light-button border-2 border-light-button hover:text-light-background hover:border-blue-button  transform transition duration-300 ease-in-out  hover:scale-110 rounded-md" href="#">About the company</a>
+                            <a href="#" className="py-1 px-4 text-light-button border-2 bg-blue-button border-blue-button rounded-md mr-2 transform transition duration-300 ease-in-out hover:scale-110 hover:bg-gray-800 hover:border-gray-800">About the software</a> <a className="ml-2 py-1 px-4 text-light-button border-2 border-light-button hover:text-light-background hover:border-blue-button  transform transition duration-300 ease-in-out  hover:scale-110 rounded-md" href="#About-the-company" onClick={() => { navigate("/aboutUs#About-the-company") }}>About the company</a>
                         </div>
                     </div>
                 </div>

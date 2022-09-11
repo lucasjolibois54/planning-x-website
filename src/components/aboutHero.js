a1522eccc498d655036f38e2dee9f4990500deda
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-11 21:28:03 +0200

    Revert "Update aboutHero.js"
    This reverts commit 787ed4a572b04666c638e8ad8563f026b7a6056d.

--- src/components/aboutHero.js
+++ src/components/aboutHero.js
@@ -7,7 +7,7 @@ import { useEffect } from 'react';
 // markup
 const AboutHero = () => {
         useEffect(() => {
-            Aos.init({ duration: 1000 });
+            Aos.init({ duration: 2000 });
         }, []);
   return (
 

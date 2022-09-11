787ed4a572b04666c638e8ad8563f026b7a6056d
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-11 21:27:49 +0200

    Update aboutHero.js

--- src/components/aboutHero.js
+++ src/components/aboutHero.js
@@ -7,7 +7,7 @@ import { useEffect } from 'react';
 // markup
 const AboutHero = () => {
         useEffect(() => {
-            Aos.init({ duration: 2000 });
+            Aos.init({ duration: 1000 });
         }, []);
   return (
 
